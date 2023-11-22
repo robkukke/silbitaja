7832a28b35b5b9678984a0ea1ac2ddd5aa7236a3
robkukke <rkukke@tlu.ee>
2023-11-22 22:09:57 +0200

    syllabifier_trie.hpp: Refactor

--- syllabifier_trie.hpp
+++ syllabifier_trie.hpp
@@ -3,34 +3,41 @@
 
 #include <cstring>
 
-const int kMaxSyllables=32;
+const int kMaxSyllables = 32;
 
-class TrieNode
-{
+class TrieNode {
 public:
-   TrieNode();
-   ~TrieNode();
-   int valueOf(char *pWord, char *pRuleToo);
-   void add(char *pWord, char val, char ruleToo);
-   static char mapChar(unsigned char c);
+    TrieNode();
+
+    ~TrieNode();
+
+    int valueOf(char *pWord, char *pRuleToo);
+
+    void add(char *pWord, char val, char ruleToo);
+
+    static char mapChar(unsigned char c);
+
 private:
-   TrieNode **fpArray;
-   char fValue;
-   char fRuleToo;
+    TrieNode **fpArray;
+    char fValue;
+    char fRuleToo;
 };
 
-class Trie
-{
+class Trie {
 public:
-   Trie();
-   ~Trie();
-   void init(const char *fileName);
-   int valueOf(char *pWord, char *pRuleToo);
-   int shortestWordLength() const;
-private:
-   TrieNode *fpRoot;
-   int fShortestWordLength;
-};
+    Trie();
 
+    ~Trie();
+
+    void init(const char *filename);
+
+    int valueOf(char *pWord, char *pRuleToo);
+
+    int shortestWordLength() const;
+
+private:
+    TrieNode *fpRoot;
+    int fShortestWordLength;
+};
 
 #endif
