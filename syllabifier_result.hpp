617038e5b230d0b9a68bd83b8c71c4f01b91a39d
robkukke <rkukke@tlu.ee>
2023-11-22 16:47:22 +0200

    syllabifier_result.hpp: Remove Linux port and C++ checks

--- syllabifier_result.hpp
+++ syllabifier_result.hpp
@@ -1,12 +1,6 @@
 #ifndef __SYLL_EXP_H__
 #define __SYLL_EXP_H__
 
-#ifdef LINUX_PORT
-#if !defined(_stdcall)
-#define _stdcall
-#endif
-#endif
-
 const int kResultLength=128;
 
 struct SyllabificationResult
@@ -15,27 +9,20 @@ struct SyllabificationResult
    int numSyllables;
    SyllabificationResult *next;
    int lastPlace;
-#ifdef __cplusplus
+
    SyllabificationResult();
    SyllabificationResult(SyllabificationResult *pRes);
-#endif
 };
 
 
-#ifdef __cplusplus
 extern "C" {
-#endif
 
 SyllabificationResult* _stdcall syllabify(char *pWord);
 
 void _stdcall deleteResults(SyllabificationResult *pFirst);
 
-#ifdef LINUX_PORT
 void initSyllabification();
-#endif
 
-#ifdef __cplusplus
 }
-#endif
 
 #endif
